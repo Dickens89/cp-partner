492470823902c0291fa627941d2fb1977a7e1c89
yizhou_homestead <15120073241@126.com>
2019-09-23 17:12:35 +0800

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -11,4 +12,11 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
+
+//    Laravel 会自动注入与请求 URI 中传入的 ID 对应的用户模型实例 隐性路由模型绑定
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
 }
