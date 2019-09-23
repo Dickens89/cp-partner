40e162d971b5e60f8aee2f1494f918aef521b1fa
yizhou_homestead <15120073241@126.com>
2019-09-23 12:45:31 +0800

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>幻想工场 合作方平台</h1>
+        <p class="lead">
+            你现在所看到的是 <a href="">听书合作方</a> 的平台主页。
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
