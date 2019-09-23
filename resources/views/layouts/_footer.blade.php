39fd2f700f953b9c5b64d469839555345865bfe1
yizhou_homestead <15120073241@126.com>
2019-09-23 14:23:03 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    {{--<img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">--}}
+    <a href="http://ihxgc.com" target=_blank>
+        幻想工场 合作方平台
+    </a>
+
+    <div class="float-right">
+        <a href="/about" >关于</a>
+    </div>
+</footer>
