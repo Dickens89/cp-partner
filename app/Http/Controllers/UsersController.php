e64e3776f579bb53f2a897167cb62a438151f1f1
yizhou_homestead <15120073241@126.com>
2019-09-24 20:38:06 +0800

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -28,7 +28,10 @@ class UsersController extends Controller
 //    Laravel 会自动注入与请求 URI 中传入的 ID 对应的用户模型实例 隐性路由模型绑定
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        $statuses = $user->statuses()
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+        return view('users.show', compact('user', 'statuses'));
     }
 
     public function store(Request $request)
