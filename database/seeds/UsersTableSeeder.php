ac2a688ca809b05a296f17b48e562d3d986280f6
yizhou_homestead <15120073241@126.com>
2019-09-24 17:58:26 +0800

    管理员可以删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,8 +16,10 @@ class UsersTableSeeder extends Seeder
         User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
 
         $user = User::find(1);
-        $user->name = 'Summer';
-        $user->email = 'summer@example.com';
+        $user->name = 'yizhou';
+        $user->email = 'yizhou1989126@126.com';
+        $user->is_admin = true;
         $user->save();
+
     }
 }
